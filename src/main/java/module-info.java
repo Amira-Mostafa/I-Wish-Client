8965a33cce860516a9e7611f64eae528711decd2
SalmaAlgayar <salmaalgayar47@gmail.com>
2026-02-03 14:46:46 +0200

    Reformatting
    Removed comments from module-info.java to clean up code.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,14 +6,11 @@ module com.example.client {
 
     exports com.example.client;
 
-    // FXML controllers
     opens com.example.client.controllers to javafx.fxml;
 
-    // If you reference these packages from other modules (usually not required unless shared):
     exports com.example.client.services;
     exports com.example.client.network;
     exports com.example.client.utils;
 
-    // If models are inside Client project:
     exports com.example.models;
 }
